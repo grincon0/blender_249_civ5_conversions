f3de1cf3a3e26992d1a79f80b06b0130e1be0cf9
Heron Wake <georgerincon2@gmail.com>
2023-05-15 21:22:22 -0400

    more bug fixes

--- python2x/add_bones_plugin.py
+++ python2x/add_bones_plugin.py
@@ -5,10 +5,15 @@ bone_names = ["adj", "CHARACTER_REORIENT", "BIP"]
 
 # Get the active armature object
 armature = Blender.Object.GetSelected()[0]
-armature.makeEditable()
+
+# Enter EditMode
+Blender.Window.EditMode(1)
+
+# Get the armature data
+armature_data = armature.getData()
 
 # Get the root bone of the armature
-root_bone = armature.getData().bones.values()[0]
+root_bone = armature_data.bones.values()[0]
 
 # Create the three new bones
 bones = []
@@ -21,19 +26,22 @@ for name in bone_names:
 root_bone_name = root_bone.name
 
 # Set adj as a child of the root bone
-bones[0].parent = root_bone_name
+bones[0].makeEditable(armature_data)
+bones[0].makeParent(root_bone_name)
 
 # Set CHARACTER_REORIENT as a child of adj
-bones[1].parent = bone_names[0]
+bones[1].makeEditable(armature_data)
+bones[1].makeParent(bone_names[0])
 
 # Set BIP as a child of CHARACTER_REORIENT
-bones[2].parent = bone_names[1]
+bones[2].makeEditable(armature_data)
+bones[2].makeParent(bone_names[1])
 
 # Update the armature
-armature.update()
-armature.makeDisplayList()
+armature_data.update()
 
-# Switch back to object mode
-Blender.Scene.GetCurrent().update()
+# Exit EditMode
+Blender.Window.EditMode(0)
 
 print("Bones added successfully!")
+
